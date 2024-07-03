ab4f50f9d939bdbacd5495f1b92434eacadf24e3
Z <mazz.4seven@gmail.com>
2024-07-03 10:42:14 +0300

    update mix file

--- mix.exs
+++ mix.exs
@@ -1,17 +1,27 @@
 defmodule Vite.MixProject do
   use Mix.Project
 
+  @version "0.1.0"
+  @source_url "https://github.com/mazz-seven/elixir-vite"
+
   def project do
     [
       app: :vite,
-      version: "0.1.0",
-      elixir: "~> 1.17",
-      start_permanent: Mix.env() == :prod,
-      deps: deps()
+      version: @version,
+      elixir: "~> 1.11",
+      deps: deps(),
+      description: "Mix tasks for invoking vite",
+      package: [
+        links: %{"GitHub" => @source_url, "vite" => "https://vitejs.dev/"},
+        licenses: ["MIT"]
+      ],
+      docs: [
+        source_url: @source_url,
+        source_ref: "v#{@version}"
+      ]
     ]
   end
 
-  # Run "mix help compile.app" to learn about applications.
   def application do
     [
       extra_applications: [:logger, inets: :optional, ssl: :optional],
@@ -20,11 +30,9 @@ defmodule Vite.MixProject do
     ]
   end
 
-  # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
+      {:ex_doc, ">= 0.0.0", only: :docs}
     ]
   end
 end
